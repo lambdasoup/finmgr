fc94add1e786e7c637c360ef50bba67ba218002c
Max Hille <mh@lambdasoup.com>
2018-02-13 15:09:07 +0100

    move into elm-src

--- genproto.sh
+++ genproto.sh
@@ -1,6 +1,6 @@
 protoc \
   --go_out=plugins=grpc:. \
-  --elm_out=plugins=grpc:. \
+  --elm_out=plugins=grpc:elm-src \
   --plugin=protoc-gen-js_service=./node_modules/.bin/protoc-gen-js_service \
   --js_out=import_style=commonjs,binary:. \
   --js_service_out=. \
